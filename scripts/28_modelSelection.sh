9865a7d6fc23e7fb55aa79a1bedeadd3d5590707
Filippo Nicolini <filippo.nicolini6@unibo.it>
2024-05-08 10:11:52 +0200

    add model selection

---
+++ scripts/28_modelSelection.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir 12_model_selection
+
+MODELS="$(tail -n +2 00_input/aa_subst_models.tsv | awk -F "\t" '{print $2}' | tr $'\n' ',' | sed -E 's/,$//')"
+
+echo "Running IQTREE ModelFinder on trimmed amino acids..."
+for i in 09_orthogroup_alignments_withoutSgloAmar/*trim.fna; do
+
+	FILENAME="$(basename "${i%%.*}")".faa
+
+	iqtree2 -s "${i%%.*}".faa -m TESTONLY --mset "$MODELS" -T 15 --prefix 12_model_selection/"$FILENAME"
+
+done
+
+for i in 11_SRG_alignments/*trim.fna; do
+
+	FILENAME="$(basename "${i%%.*}")".faa
+
+	iqtree2 -s "${i%%.*}".faa -m TESTONLY --mset "$MODELS" -T 15 --prefix 12_model_selection/"$FILENAME"
+
+done
